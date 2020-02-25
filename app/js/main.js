2324d85307ecd8112f71e2d6d8e123c8cc4926c6
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2020-02-25 20:05:56 +0200

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,19 @@ $(function() {
 
     var mixer = mixitup('.products__inner-box');
 
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true
+
+    })
+
+
 });
