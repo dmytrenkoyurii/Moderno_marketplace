78d621a5507108882a2fbd5d102a24af97956e81
dmytrenkoyurii <dmitrenko.yura@gmail.com>
2020-03-04 18:59:59 +0200

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function() {
 
-    var mixer = mixitup('.products__inner-box');
-
     $(".rate-star").rateYo({
         rating: 4.5,
         starWidth: "12px",
@@ -14,7 +12,31 @@ $(function() {
         slidesToScroll: 4,
         dots: true
 
-    })
+    });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        grid: true,
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+
+    });
+
+    $('.icon-th-list').on('click', function() {
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+
+    $('.icon-th-large').on('click', function() {
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
+
+    var mixer = mixitup('.products__inner-box');
 
 });
